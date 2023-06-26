6f980103b11935941986df1f476ffe242c1679da
Christophe Bornet <cbornet@hotmail.com>
2023-06-26 10:57:33 +0200

    Add a module pulsar-ai-functions to have a distinct NAR for AI ops (#85)

--- pulsar-transformations/src/test/java/com/datastax/oss/pulsar/functions/transforms/TransformFunctionTest.java
+++ pulsar-transformations/src/test/java/com/datastax/oss/pulsar/functions/transforms/TransformFunctionTest.java
@@ -17,28 +17,13 @@ package com.datastax.oss.pulsar.functions.transforms;
 
 import static com.datastax.oss.pulsar.functions.transforms.Utils.assertNonOptionalField;
 import static com.datastax.oss.pulsar.functions.transforms.Utils.assertOptionalField;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertNotNull;
 import static org.testng.Assert.assertThrows;
 import static org.testng.AssertJUnit.assertNull;
 
-import com.azure.ai.openai.OpenAIClient;
-import com.azure.ai.openai.models.ChatCompletions;
-import com.azure.ai.openai.models.ChatCompletionsOptions;
-import com.datastax.oss.pulsar.functions.transforms.datasource.QueryStepDataSource;
-import com.datastax.oss.pulsar.functions.transforms.model.config.DataSourceConfig;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
-import java.util.List;
 import java.util.Map;
-import org.apache.avro.generic.GenericArray;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.util.Utf8;
 import org.apache.pulsar.client.api.Schema;
@@ -53,7 +38,6 @@ import org.apache.pulsar.common.schema.SchemaInfo;
 import org.apache.pulsar.common.schema.SchemaType;
 import org.apache.pulsar.functions.api.Context;
 import org.apache.pulsar.functions.api.Record;
-import org.mockito.ArgumentCaptor;
 import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -132,41 +116,6 @@ public class TransformFunctionTest {
       },
       {
         "{'steps': [{'type': 'compute', 'fields': [{'name': 'value.field1', expression: 'value.field1', type: 'DECIMAL'}]}]}"
-      },
-      {"{'steps': [], 'openai': {'access-key': 'qwerty', 'url': 'some-url', 'provider': 'azure'}}"},
-      {"{'steps': [], 'openai': {'access-key': 'qwerty'}}"},
-      {
-        "{'steps': [{'type': 'compute-ai-embeddings', 'text': '{{ value }}', 'embeddings-field': 'emb', 'model': 'the-new-model'}]}"
-      },
-      {
-        "{"
-            + "'steps': ["
-            + "  {"
-            + "    'type': 'ai-chat-completions',"
-            + "    'model': 'example_model',"
-            + "    'messages': ["
-            + "      {"
-            + "        'role': 'user',"
-            + "        'content': 'Hello'"
-            + "      }"
-            + "    ],"
-            + "    'max-tokens': 100,"
-            + "    'temperature': 0.8,"
-            + "    'top-p': 0.9,"
-            + "    'logit-bias': {"
-            + "      'negative': -1"
-            + "    },"
-            + "    'user': 'John',"
-            + "    'stop': ['bye', 'stop'],"
-            + "    'presence-penalty': 0.5,"
-            + "    'frequency-penalty': 0.2"
-            + "  }"
-            + "],"
-            + "'openai': {'access-key': 'qwerty'}"
-            + "}"
-      },
-      {
-        "{'steps': [{'type': 'ai-chat-completions', 'model': 'example_model', 'messages': [{'role': 'user','content': 'Hello'}]}], 'openai': {'access-key': 'qwerty'}}"
       }
     };
   }
@@ -254,34 +203,11 @@ public class TransformFunctionTest {
       {
         "{'steps': [{'type': 'compute', 'fields': [{'name': 'key', expression: '1234', type: 'DATE'}]}]}"
       },
-      {"{'steps': [], 'openai': {'url': 'some-url'}}"},
-      {"{'steps': [], 'openai': {'provider': 'invalid'}}"},
       {
-        "{'steps': [{'type': 'compute-ai-embeddings', 'text': '{{ value }}', 'embeddings-field': 'emb'}]}"
+        "{'steps': [{'type': 'compute-ai-embeddings', 'text': '{{ value }}', 'embeddings-field': 'emb', 'model': 'the-new-model'}]}"
       },
       {
-        "{'steps': [{'type': 'compute-ai-embeddings', 'text': '{{ value }}', 'model': 'the-new-model'}]}"
-      },
-      {
-        "{'steps': [{'type': 'compute-ai-embeddings', 'embeddings-field': 'emb', 'model': 'the-new-model'}]}"
-      },
-      {
-        "{'steps': [{'type': 'ai-chat-completions', 'model': 'example_model', 'messages': [{'role': 'user','content': 'Hello'}]}]}"
-      },
-      {
-        "{'steps': [{'type': 'ai-chat-completions', 'messages': [{'role': 'user','content': 'Hello'}]}], 'openai': {'access-key': 'qwerty'}}"
-      },
-      {
-        "{'steps': [{'type': 'ai-chat-completions', 'model': 'example_model'}], 'openai': {'access-key': 'qwerty'}}"
-      },
-      {
-        "{'steps': [{'type': 'ai-chat-completions', 'model': 'example_model', 'messages': [{'role': 'invalid','content': 'Hello'}]}], 'openai': {'access-key': 'qwerty'}}"
-      },
-      {
-        "{'steps': [{'type': 'ai-chat-completions', 'model': 'example_model', 'messages': [{'content': 'Hello'}]}], 'openai': {'access-key': 'qwerty'}}"
-      },
-      {
-        "{'steps': [{'type': 'ai-chat-completions', 'model': 'example_model', 'messages': [{'role': 'user'}]}], 'openai': {'access-key': 'qwerty'}}"
+        "{'steps': [{'type': 'ai-chat-completions', 'model': 'example_model', 'messages': [{'role': 'user','content': 'Hello'}]}], 'openai': {'access-key': 'qwerty'}}"
       }
     };
   }
@@ -561,119 +487,6 @@ public class TransformFunctionTest {
     };
   }
 
-  @Test
-  void testChatCompletions() throws Exception {
-    String userConfig =
-        (""
-                + "{"
-                + "  'steps': ["
-                + "    {"
-                + "      'type': 'ai-chat-completions',"
-                + "      'model': 'test-model',"
-                + "      'messages': ["
-                + "        {"
-                + "          'role': 'user',"
-                + "          'content': '{{ value.valueField1 }} {{ key.keyField2 }}'"
-                + "        }"
-                + "      ]"
-                + "    }"
-                + "  ]"
-                + "}")
-            .replace("'", "\"");
-    Map<String, Object> config =
-        new Gson().fromJson(userConfig, new TypeToken<Map<String, Object>>() {}.getType());
-    TransformFunction transformFunction = spy(new TransformFunction());
-
-    OpenAIClient client = mock(OpenAIClient.class);
-
-    String completion =
-        (""
-                + "{"
-                + "  'choices': ["
-                + "    {"
-                + "      'message': {"
-                + "        'content': 'result',"
-                + "        'role': 'user'"
-                + "      }"
-                + "    }"
-                + "  ]"
-                + "}")
-            .replace("'", "\"");
-    when(client.getChatCompletions(eq("test-model"), any()))
-        .thenReturn(new ObjectMapper().readValue(completion, ChatCompletions.class));
-    when(transformFunction.buildOpenAIClient(any())).thenReturn(client);
-
-    Record<GenericObject> record = Utils.createTestAvroKeyValueRecord();
-    Utils.TestContext context = new Utils.TestContext(record, config);
-    transformFunction.initialize(context);
-    transformFunction.process(record.getValue(), context);
-
-    ArgumentCaptor<ChatCompletionsOptions> captor =
-        ArgumentCaptor.forClass(ChatCompletionsOptions.class);
-    verify(client).getChatCompletions(eq("test-model"), captor.capture());
-
-    assertEquals(captor.getValue().getMessages().get(0).getContent(), "value1 key2");
-  }
-
-  @Test
-  void testChatCompletionsWithLogField() throws Exception {
-    String userConfig =
-        (""
-                + "{"
-                + "  'steps': ["
-                + "    {"
-                + "      'type': 'ai-chat-completions',"
-                + "      'model': 'test-model',"
-                + "      'completion-field': 'value.completion',"
-                + "      'log-field': 'value.log',"
-                + "      'messages': ["
-                + "        {"
-                + "          'role': 'user',"
-                + "          'content': '{{ value.valueField1 }} {{ key.keyField2 }}'"
-                + "        }"
-                + "      ]"
-                + "    }"
-                + "  ]"
-                + "}")
-            .replace("'", "\"");
-    Map<String, Object> config =
-        new Gson().fromJson(userConfig, new TypeToken<Map<String, Object>>() {}.getType());
-    TransformFunction transformFunction = spy(new TransformFunction());
-
-    OpenAIClient client = mock(OpenAIClient.class);
-
-    String completion =
-        (""
-                + "{"
-                + "  'choices': ["
-                + "    {"
-                + "      'message': {"
-                + "        'content': 'result',"
-                + "        'role': 'user'"
-                + "      }"
-                + "    }"
-                + "  ]"
-                + "}")
-            .replace("'", "\"");
-    when(client.getChatCompletions(eq("test-model"), any()))
-        .thenReturn(new ObjectMapper().readValue(completion, ChatCompletions.class));
-    when(transformFunction.buildOpenAIClient(any())).thenReturn(client);
-
-    Record<GenericObject> record = Utils.createTestAvroKeyValueRecord();
-    Utils.TestContext context = new Utils.TestContext(record, config);
-    transformFunction.initialize(context);
-    Record<?> outputRecord = transformFunction.process(record.getValue(), context);
-
-    KeyValueSchema<?, ?> messageSchema = (KeyValueSchema<?, ?>) outputRecord.getSchema();
-    KeyValue<?, ?> messageValue = (KeyValue<?, ?>) outputRecord.getValue();
-    GenericData.Record valueAvroRecord =
-        Utils.getRecord(messageSchema.getValueSchema(), (byte[]) messageValue.getValue());
-    assertEquals("result", valueAvroRecord.get("completion").toString());
-    assertEquals(
-        "{\"options\":{\"messages\":[{\"role\":\"user\",\"content\":\"value1 key2\"}],\"max_tokens\":null,\"temperature\":null,\"top_p\":null,\"logit_bias\":null,\"user\":null,\"n\":null,\"stop\":null,\"presence_penalty\":null,\"frequency_penalty\":null,\"stream\":null,\"model\":null},\"model\":\"test-model\"}",
-        valueAvroRecord.get("log").toString());
-  }
-
   // TODO: just for demo. To be removed
   @Test
   void testRemoveMergeAndToString() throws Exception {
@@ -699,62 +512,5 @@ public class TransformFunctionTest {
         outputRecord.getValue(),
         "{\"keyField2\": \"key2\", \"keyField3\": \"key3\", \"valueField1\": "
             + "\"value1\", \"valueField2\": \"value2\", \"valueField3\": \"value3\"}");
-  }
-
-  @Test
-  void testQuery() throws Exception {
-    String userConfig =
-        (""
-                + "{'datasource': {'service': 'mock','username': 'test','password': 'testpwd', 'secureBundle':'xx'},"
-                + "   'steps': ["
-                + "    {'type': 'query', 'fields': ['key.keyField1'], 'query':'select * from products where description like ?', 'output-field':'results'}"
-                + "]}")
-            .replace("'", "\"");
-    Map<String, Object> config =
-        new Gson().fromJson(userConfig, new TypeToken<Map<String, Object>>() {}.getType());
-    TransformFunction transformFunction =
-        new TransformFunction() {
-          @Override
-          protected QueryStepDataSource buildDataSource(DataSourceConfig dataSourceConfig) {
-            assertEquals(dataSourceConfig.getService(), "mock");
-            assertEquals(dataSourceConfig.getUsername(), "test");
-            assertEquals(dataSourceConfig.getPassword(), "testpwd");
-            assertEquals(dataSourceConfig.getSecureBundle(), "xx");
-
-            return new QueryStepDataSource() {
-              @Override
-              public List<Map<String, String>> fetchData(String query, List<Object> params) {
-                assertEquals("select * from products where description like ?", query);
-                assertEquals(params.size(), 1);
-                assertEquals(params.get(0), "key1");
-
-                return List.of(
-                    Map.of("productId", "1", "name", "Product1"),
-                    Map.of("productId", "2", "name", "Product2"));
-              }
-            };
-          }
-        };
-
-    Record<GenericObject> record = Utils.createTestAvroKeyValueRecord();
-    Utils.TestContext context = new Utils.TestContext(record, config);
-    transformFunction.initialize(context);
-    Record<?> outputRecord = transformFunction.process(record.getValue(), context);
-
-    KeyValueSchema<?, ?> messageSchema = (KeyValueSchema<?, ?>) outputRecord.getSchema();
-    KeyValue<?, ?> messageValue = (KeyValue<?, ?>) outputRecord.getValue();
-
-    GenericData.Record valueAvroRecord =
-        Utils.getRecord(messageSchema.getValueSchema(), (byte[]) messageValue.getValue());
-    Object results = valueAvroRecord.get("results");
-    assertNotNull(results);
-    GenericArray array = (GenericArray) results;
-    assertEquals(2, array.size());
-    assertEquals(
-        Map.of(new Utf8("productId"), new Utf8("1"), new Utf8("name"), new Utf8("Product1")),
-        array.get(0));
-    assertEquals(
-        Map.of(new Utf8("productId"), new Utf8("2"), new Utf8("name"), new Utf8("Product2")),
-        array.get(1));
   }
 }
