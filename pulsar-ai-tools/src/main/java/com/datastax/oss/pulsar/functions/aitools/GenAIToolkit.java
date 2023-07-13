45c1564e9b32f996b7111fafdff0dce9a6b5af60
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-07-13 10:14:33 +0200

    Use a different classname for the ai-tools Function (com.datastax.oss.pulsar.functions.aitools.GenAIToolkit)

---
+++ pulsar-ai-tools/src/main/java/com/datastax/oss/pulsar/functions/aitools/GenAIToolkit.java
@@ -0,0 +1,10 @@
+package com.datastax.oss.pulsar.functions.aitools;
+
+import com.datastax.oss.pulsar.functions.transforms.TransformFunction;
+
+/**
+ * This is a dummy class to allow having a different classname for the "ai-tools" function.
+ * In this module you can find a different config-schema.yaml file that unlocks the "ai-tools" function.
+ */
+public class GenAIToolkit extends TransformFunction {
+}
