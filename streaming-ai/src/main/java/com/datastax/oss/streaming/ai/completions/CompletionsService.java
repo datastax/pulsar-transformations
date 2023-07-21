617032a8e4d4d1128570e71b5eeebdec1b798684
Enrico Olivelli <enrico.olivelli@datastax.com>
2023-07-21 14:01:39 +0200

    remove useless class

--- streaming-ai/src/main/java/com/datastax/oss/streaming/ai/completions/CompletionsService.java
+++ streaming-ai/src/main/java/com/datastax/oss/streaming/ai/completions/CompletionsService.java
@@ -21,11 +21,4 @@ import com.azure.ai.openai.models.ChatCompletionsOptions;
 public interface CompletionsService {
   ChatCompletions getChatCompletions(String model, ChatCompletionsOptions chatCompletionsOptions);
 
-  public static class NoopCompletionService implements CompletionsService {
-    @Override
-    public ChatCompletions getChatCompletions(
-        String model, ChatCompletionsOptions chatCompletionsOptions) {
-      throw new UnsupportedOperationException();
-    }
-  }
 }
